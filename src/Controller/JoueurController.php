8f99292f7d911dc8804b67a75bc08be4d55b2680
oussamaassal <ahmedoussama.assal@esprit.tn>
2024-03-30 04:19:42 +0100

    Affichage et suppression ajouté

--- src/Controller/JoueurController.php
+++ src/Controller/JoueurController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\JoueurRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +13,29 @@ class JoueurController extends AbstractController
     #[Route('/joueur', name: 'app_joueur')]
     public function index(): Response
     {
-        return $this->render('/admin/index.html.twig', [
+        return $this->render('list.html.twig', [
             'controller_name' => 'JoueurController',
         ]);
+    }
+
+    #[Route('/getJoueurs', name: 'Joueur_list')]
+    public function listJoueurs(JoueurRepository $repo):Response{
+
+        $list = $repo->findAll();
+        return $this->render('joueur/listJoueur.html.twig',[
+            'list' => $list
+        ]);
+    }
+
+    #[Route('/deleteJoueur/{id}', name: 'Joueur_delete')]
+    public function deleteBook(ManagerRegistry $manager , JoueurRepository $repo, $id):Response{
+        $em = $manager->getManager();
+
+        $Joueur = $repo->find($id);
+
+        $em->remove($Joueur);
+        $em->flush();
+        
+        return $this->redirectToRoute('Joueur_list');
     }
 }
