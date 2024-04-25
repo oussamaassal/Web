01a83df1dacec3fc4d7cf45023a2bec0bc6211ef
oussamaassal <ahmedoussama.assal@esprit.tn>
2024-04-25 10:12:07 +0100

    qr code bundle added

--- src/Controller/JoueurController.php
+++ src/Controller/JoueurController.php
@@ -10,6 +10,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
 
 class JoueurController extends AbstractController
 {
