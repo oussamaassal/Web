fa1ef9a26d354b621085e115c169e292ab89fbc6
moumn <houssem.moumni@esprit.tn>
2024-05-01 18:49:17 +0100

    Fix SMS Bundle

--- src/Service/SmsGenerator.php
+++ src/Service/SmsGenerator.php
@@ -11,7 +11,7 @@ class SmsGenerator
     {
         
         $accountSid ='AC7f42e1dfeb9bfc83506676b133dd09c6';  //Identifiant du compte twilio
-        $authToken ='1052fc23b9c0561ab209cd0616c69168'; //Token d'authentification
+        
         $fromNumber ='+12196820814'; // Numéro de test d'envoie sms offert par twilio
 
         $toNumber = $number; // Le numéro de la personne qui reçoit le message
